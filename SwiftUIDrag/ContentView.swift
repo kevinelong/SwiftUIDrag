14d35b15c5051ef4e224118fd4990d6b713994e5
kevinelong2 <kevinelong@gmail.com>
2019-08-08 10:22:37 -0500

    Initial Commit

---
+++ SwiftUIDrag/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIDrag
+//
+//  Created by Kevin Ernest Long on 8/8/19.
+//  Copyright © 2019 Clever Clever Consulting Partners. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
