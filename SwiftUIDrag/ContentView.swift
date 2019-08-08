154ce1e998bcd0b18cbc139a554c68309dc80de5
kevinelong2 <kevinelong@gmail.com>
2019-08-08 13:19:32 -0500

    new files for image drag

--- SwiftUIDrag/ContentView.swift
+++ SwiftUIDrag/ContentView.swift
@@ -8,9 +8,48 @@
 
 import SwiftUI
 
+//extension View {
+//    public func offset(by offset: CGPoint) -> ModifiedContent<View, _OffsetEffect>{
+//    self.offset(x: offset.x, y: offset.y)
+//  }
+//}
+
 struct ContentView: View {
+    @State private var viewState = CGSize.zero
+    @State var gestureStartLocation: CGPoint = .zero
+//    @State var lastGestureDirection: GameLogic.Direction = .up
+    @State var imageLocation: CGPoint = .zero
+
     var body: some View {
-        Text("Hello World")
+        Image("log").resizable()
+        .position(imageLocation)
+//            .offset(by: self.position)
+        .frame(width: 32.0, height: 32.0)
+            .gesture(
+                DragGesture(minimumDistance:10).onChanged { v in
+                    print(v)
+                    
+                    guard self.gestureStartLocation != v.startLocation else { return }
+                    print(self.gestureStartLocation)
+                    
+                    self.imageLocation = v.location
+                        
+                    withTransaction(Transaction()) {
+                        self.gestureStartLocation = v.startLocation
+                    }
+                    self.setNeedsDisplay()
+                }
+                .onEnded { value in
+                    self.imageLocation = value.location
+
+//                    if self.viewState.height > 200 {
+//                        self.viewState = CGSize(width: 0, height: 1200)
+//                    } else {
+//                        self.viewState = .zero
+//                    }
+        
+            }
+        )
     }
 }
 
